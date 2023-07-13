c28572093cd9273c2fa6853bac04b940319f1ea5
BaoDinh <winterheartlove18@gmail.com>
2023-07-13 16:53:54 +0700

    Fix download log file api

--- src/files/file.controller.ts
+++ src/files/file.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Res, StreamableFile } from '@nestjs/common';
+import { Controller, Get, Res, StreamableFile } from '@nestjs/common';
 import { FileService } from './file.service';
 import { Response } from 'express';
 
@@ -7,11 +7,15 @@ export class FileController {
     constructor(
         private fileService: FileService
     ){}
-    @Get('/log/:logType')
-    downloadLogFile(@Param('logType') logType: string, @Res() response : Response){
-        const file = this.fileService.downloadLogFile(logType);
-        response.contentType('text/plain');
-        response.attachment();
+    @Get('/log')
+    downloadLogFile(@Res() response : Response){
+        const { file, filename } = this.fileService.downloadLogFile();
+        response.set({
+            'Content-Type': 'application/zip',
+            'Content-Disposition': `attachment; filename="${filename}"`,
+        })
+        // file.pipe(response);
         response.send(file);
+        
     }
 }
